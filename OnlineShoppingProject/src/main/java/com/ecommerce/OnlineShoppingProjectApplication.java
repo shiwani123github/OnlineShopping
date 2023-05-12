7cdaaaa60887a5b4826680db290ca1145a9b4aee
Balraj <balrajnaik4200@gmail.com>
2023-05-12 11:59:27 +0530

    Added new line

--- OnlineShoppingProject/src/main/java/com/ecommerce/OnlineShoppingProjectApplication.java
+++ OnlineShoppingProject/src/main/java/com/ecommerce/OnlineShoppingProjectApplication.java
@@ -9,6 +9,7 @@ public class OnlineShoppingProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShoppingProjectApplication.class, args);
 		System.out.println("this is demo");
+		System.out.println("Hello this is from balraj side");
 	}
 
 }
