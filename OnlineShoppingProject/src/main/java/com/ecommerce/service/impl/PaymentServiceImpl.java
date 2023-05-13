7f00da74f5c226432558f0edcc3baa02d2131456
Shiwani <shiwanidewang54@gmail.com>
2023-05-12 21:39:11 -0700

    update operation done

---
+++ OnlineShoppingProject/src/main/java/com/ecommerce/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ecommerce.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.model.Payment;
+import com.ecommerce.repository.PaymentRepository;
+import com.ecommerce.service.PaymentService;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+	@Autowired
+	private PaymentRepository paymentRepository;
+
+	@Override
+	public Payment updatePayment(Payment payment) {
+		Integer id = payment.getId();
+		Payment pay = paymentRepository.findById(id).get();
+		pay.setCardNumber(payment.getCardNumber());
+		pay.setNameOnCard(payment.getNameOnCard());
+		pay.setPmtDate(payment.getPmtDate());
+		pay.setAmount(payment.getAmount());
+		pay.setPaymentMethod(payment.getPaymentMethod());
+		pay.setCvv(payment.getCvv());
+		pay.setExMonth(payment.getExMonth());
+		pay.setExYear(payment.getExYear());
+		Payment payment1 = paymentRepository.save(pay);
+		return payment1;
+	}
+
+	@Override
+	public Payment savePayment(Payment payment) {
+		Payment pay = paymentRepository.save(payment);
+		return pay;
+	}
+
+}
